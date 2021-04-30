68a58aed4cf6b9cece5968fd27476cb09825086b
suryaharahap <suryaharahap18@gmail.com>
2021-04-30 15:42:17 +0700

    initial commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Surya on 30/04/21.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
