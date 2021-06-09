9d3cb648b6918e6dcc0e32682e174e3122c81b89
suryaharahap <suryaharahap18@gmail.com>
2021-06-09 16:16:48 +0700

    Using SPM(Swift Package Manager

---
+++ NewsApp/Views/ArticleView.swift
@@ -0,0 +1,21 @@
+//
+//  ArticleView.swift
+//  NewsApp
+//
+//  Created by Surya on 09/06/21.
+//
+
+import SwiftUI
+import URLImage
+
+struct ArticleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ArticleView_Previews: PreviewProvider {
+    static var previews: some View {
+        ArticleView()
+    }
+}
